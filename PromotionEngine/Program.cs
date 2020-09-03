a12eb7903fa17c5a113ffc82d2fd140d58715942
hariomvs <hariomacet@gmail.com>
2020-09-03 14:31:07 +0530

    first commit

---
+++ PromotionEngine/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Channels;
+
+namespace PromotionEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Press q to quit");
+
+            bool quitFlag = false;
+            while (!quitFlag)
+            {
+                Console.WriteLine("Please enter SKU IDs.");
+                // string userInput;
+                string SKuInputs = Console.ReadLine();
+                if (SKuInputs == "q")
+                {
+                    quitFlag = true;
+                }
+                else
+                {
+                    Promotion.GetPromotions(SKuInputs);
+                    Console.ReadLine();
+                }
+            }
+           
+        }
+
+
+       
+
+
+
+    }
+}
